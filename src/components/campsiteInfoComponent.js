f4a9c60a970ca578520f0637952d8934132423be
Alvin <huerox@msn.com>
2020-05-30 12:27:14 -0700

    Updated the view of the  CampsiteInfo component to display the list of comments about the campsite using the Bootstrap.

--- src/components/campsiteInfoComponent.js
+++ src/components/campsiteInfoComponent.js
@@ -16,10 +16,24 @@ class CampsiteInfo extends Component {
         );
     }
 
+    renderComments(comments){
+        if(comments){
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment => <div key={comment.id}>{comment.text}<br/>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>)}   
+                </div>
+            );
+        } else {
+            return <div></div>
+        }
+    }
+
     render(){
         if(this.props.campsite){
             return<div className="row">
                 {this.renderCampsite(this.props.campsite)}
+                {this.renderComments(this.props.campsite.comments)}
             </div>
         } else {
             return <div></div>
