2de57ff57f3b1cd8c3119ce6415eb5b68991304b
Alvin <huerox@msn.com>
2020-05-30 12:38:54 -0700

    attempted to reformat date to add space after each text comment.

--- src/components/campsiteInfoComponent.js
+++ src/components/campsiteInfoComponent.js
@@ -21,7 +21,7 @@ class CampsiteInfo extends Component {
             return (
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    {comments.map(comment => <div key={comment.id}>{comment.text}<br/>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>)}   
+                    {comments.map(comment => <div key={comment.id}>{comment.text}<br />--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}<br /></div>)}   
                 </div>
             );
         } else {
