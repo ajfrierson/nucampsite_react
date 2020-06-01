108e81f9c645fb2283a43ecabf88784e413179e1
Alvin <huerox@msn.com>
2020-06-01 14:42:43 -0700

    Added a new component that will act as a container component between App and the rest of the components, and update an existing component to a presentational component.

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import CampsiteInfo from './campsiteInfoComponent';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+
 
 //Directory component with a method for changing the state of the campsites displayed
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-
-    // renderSeletedCampsite(campsite) {
-    //     if(campsite){
-    //         return (
-    //             <Card>
-    //                 <CardImg top src={campsite.image} alt={campsite.name} />
-    //                 <CardBody>
-    //                     <CardTitle>{campsite.name}</CardTitle>
-    //                     <CardText>{campsite.description}</CardText>
-    //                 </CardBody>
-    //             </Card>
-    //         );
-    //     }
-    //     return <div />;
-    // }
 //Card component for the campsite images and names
     render(){
         const directory = this.props.campsites.map(campsite => {
             return(
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <Card onClick={() => this.props.onClick(campsite.id)}>   
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -48,13 +23,7 @@ class Directory extends Component {
             <div className="container">
                 <div className="row">
                     {directory}
-                </div>
-                {/* <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSeletedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div> */}
-                <div><CampsiteInfo campsite={this.state.selectedCampsite}/></div>   
+                </div>  
             </div>
         );
     }
--- src/components/campsiteInfoComponent.js
+++ src/components/campsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 // Campsite component
 class CampsiteInfo extends Component {
     renderCampsite(campsite){
@@ -29,16 +29,20 @@ class CampsiteInfo extends Component {
         }
     }
 //The following code return comments and campsites to the DOM
-    render(){
-        if(this.props.campsite){
-            return<div className="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-            </div>
-        } else {
-            return <div />
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            );
         }
+        return <div />;
     }
+
 }
 
 export default CampsiteInfo; 
