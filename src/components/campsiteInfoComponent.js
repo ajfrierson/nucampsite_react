85af6d13e572b5d452a891ced5a2610a3a7ac998
Alvin <huerox@msn.com>
2020-06-02 11:57:49 -0700

    refactored the campsite info component into three functional components; and redisigned the comments to include a break after each one.

--- src/components/campsiteInfoComponent.js
+++ src/components/campsiteInfoComponent.js
@@ -16,33 +16,39 @@ function RenderCampsite({campsite}){
     );
 }
 
-//Method to render comments and date
-function RenderComments(comments){
-        if(comments){
-            return (
-                <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments.map(comment => <div key={comment.id}>{comment.text}<br />--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>)}   
-                </div>
-            );
-        } else {
-            return <div />
-        }
-    }
-//The following code return comments and campsites to the DOM
-    render() {
-        if (this.props.campsite) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
+//Functional component to render comments and date
+function RenderComments({comments}){
+    if(comments) {
+        return (
+            <div className="col-md-5 m-1">
+                <h3>Comments</h3>
+                {comments.map(comment => {
+                return (
+                    <div key={comment.id}>
+                        <p>{comment.text}<br />
+                        --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} 
+                        </p>
                     </div>
-                </div>
-            );
-        }
-        return <div />;
+                )})}
+            </div>
+        );
     }
+    return <div />
+}
+//The following code return comments and campsites to the DOM
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite}/>
+                    <RenderComments comments={props.campsite.comments}/>
+                </div>
+            </div>
+        );
+    }
+    return <div />;
+}
 
 
 
